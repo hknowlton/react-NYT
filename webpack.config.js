32022d2a7c5ba3465ea82d7f10471c7037376a96
Heather Knowlton <heatherknowlton79@gmail.com>
2017-06-07 20:32:59 -0400

    set up. landing page rendering:

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
       }
     ]
   },
+
+  resolveLoader: {
+    moduleExtensions: ['-loader']
+  },
   // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
   // Without this the console says all errors are coming from just coming from bundle.js
   devtool: "eval-source-map"
